a86bd4ba13e21e3fb354489a5b7199e339b4271e
EmuLock <kulthumeliase@gmail.com>
2019-07-13 10:15:33 +0800

    quiz works now
    fixed mistake in queston numbers

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
-    var question3 = document.quiz.question4.value;
-    var question3 = document.quiz.question5.value;
-    var question3 = document.quiz.question6.value;
-    var question3 = document.quiz.question7.value;
-    var question3 = document.quiz.question8.value;
-    var question3 = document.quiz.question9.value;
-    var question3 = document.quiz.question11.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var question6 = document.quiz.question6.value;
+    var question7 = document.quiz.question7.value;
+    var question8 = document.quiz.question8.value;
+    var question9 = document.quiz.question9.value;
+    var question11 = document.quiz.question11.value;
 	var correct = 0;
 
 
@@ -59,7 +59,7 @@ function check(){
     }
     
 	
-
+	var pictures = [ ""]
 	var messages = ["Are you sure you're not the boy who lived?You know waay too much about the Dark Lord", "The Dark Lord is pleased", "Voldemort is dissapointed but he will not kill you", "Not bad...for a filthy Mudblood", "CRUCIO" ,  "AVADA KEDAVRAAAAA"];
 	var score;
 
